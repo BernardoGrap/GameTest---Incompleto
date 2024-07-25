c191f858560fdb6564444bf5a73d9a9e03eaa0fb
Bernardo <bernardograp@gmail.com>
2024-07-25 10:36:21 -0300

    primeiro commit

---
+++ src/com/strawhat/entities/Bullet.java
@@ -0,0 +1,10 @@
+package com.strawhat.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Bullet extends Entity{
+
+	public Bullet(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+	}
+}
